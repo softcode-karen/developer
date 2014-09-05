3004a6898f2944b67c94d6d156556bffc899ea43
softcode-karen <karjan9645@gmail.com>
2014-09-05 12:29:00 +0400

    Menu controller add edit function

--- codeigniter/application/controllers/admin/menu.php
+++ codeigniter/application/controllers/admin/menu.php
@@ -8,27 +8,28 @@ class Menu extends CI_Controller {
 		$this->load->library('form_validation');
 	}
 	function add_edit_menu($id = ""){
-		//$this->form_validation->set_rules('title', 'Menu Title', 'required|min_length[3]|max_length[25]');
-		//$this->form_validation->set_rules("url","Menu seourl","required|min_length[3]|max_length[25]");
-		//if($this->form_validation->run()){
-				$title = $this->input->post("title");
-				$url = $this->input->post("url");
-				$data = array(
-					"title" => $title,
-					"url" => $url
-				);	
-			if(empty($id)){
-				$this->load->admin_view('add_edit_menu');
+		$this->form_validation->set_rules('title', 'Menu Title', 'required|min_length[3]|max_length[25]');
+		$this->form_validation->set_rules("url","Menu seourl","required|min_length[3]|max_length[25]");
+		$title = $this->input->post("title");
+		$url = $this->input->post("url");
+		$data = array(
+			"title" => $title,
+			"url" => $url
+		);	
+		if(empty($id)){
+			$this->load->admin_view('add_edit_menu');
+			if($this->form_validation->run()){
 				$this->menu_model->insert_menu($data);
-			}else{
-				$datad["query"] = $this->menu_model->select_menu_data($id);
-				$this->load->admin_view('add_edit_menu',$datad);
-				if(!empty($this->input->post("title"))){
-					$this->menu_model->update_menu($id,$data);
-				}
+				redirect("admin/home/menu");
 			}
-			//redirect("admin/home/menu");
-		//}
+		}else{
+			$datad["query"] = $this->menu_model->select_menu_data($id);
+			$this->load->admin_view('add_edit_menu',$datad);
+			if($this->form_validation->run()){
+				$this->menu_model->update_menu($id,$data);
+				redirect("admin/home/menu");
+			}
+		}
 	}
 	public function delete($id){
 		$this->menu_model->delete_menu($id);
